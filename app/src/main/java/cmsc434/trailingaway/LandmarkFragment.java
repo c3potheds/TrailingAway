53599e85c6914a377a351a2d04bff6acb5fb79eb
Simeon <simeon.anfinrud@mac.com>
2014-12-15 11:26:36 -0500

    Did the listener workaround for other buttons; untested

--- app/src/main/java/cmsc434/trailingaway/LandmarkFragment.java
+++ app/src/main/java/cmsc434/trailingaway/LandmarkFragment.java
@@ -32,33 +32,14 @@ import com.google.android.gms.maps.model.LatLng;
  *
  */
 public class LandmarkFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener _listener;
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LandmarkFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LandmarkFragment newInstance(String param1, String param2) {
+    public static LandmarkFragment newInstance() {
         LandmarkFragment fragment = new LandmarkFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -69,11 +50,6 @@ public class LandmarkFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-
-        }
     }
 
     @Override
@@ -86,6 +62,20 @@ public class LandmarkFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 onAddLandmarkSaveClick(view);
+            }
+        });
+        Button cancel = (Button) v.findViewById(R.id.buttonAddLandmarkCancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAddLandmarkCancelClick(view);
+            }
+        });
+        Button photo = (Button)v.findViewById(R.id.buttonAddPhoto);
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAddPhotoClick(view);
             }
         });
         return v;
